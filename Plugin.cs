129e73fa796a50479a73af33777d35c3a90147ea
SkyKDG <bryan.cant@student.kdg.be>
2023-07-19 04:31:36 +0200

    Ported to gloomrot - updated all the required things - fixed the broken things - only water related containers dont seem to work anymore (BuffSystem_Spawn_Server.OnUpdate is not called for using those) - added hopefully all the potions that take a bottle or a canteen.

--- Plugin.cs
+++ Plugin.cs
@@ -1,99 +1,49 @@
-﻿using BepInEx;
-using BepInEx.IL2CPP;
+﻿using BepInEx.Unity.IL2CPP;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
-using Unity.Entities;
-using UnityEngine;
-using System.IO;
-using System;
-using ProjectM;
+using BepInEx;
 using PotionsHaveContainers.Systems;
-using BepInEx.Configuration;
+using Bloodstone.API;
 
 namespace PotionsHaveContainers
 {
-    [BepInPlugin(BuildConfig.PackageID, BuildConfig.Name, BuildConfig.Version)]
-    public class Plugin : BasePlugin
-    { 
-        private Harmony harmony;
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("gg.deca.Bloodstone")]
+    public class Plugin : BasePlugin, IRunOnInitialized
+    {
+        private Harmony _harmony;
 
-        private static ConfigEntry<bool> EnableMod;
-        public static bool _EnableMod;
-        
-        public static string ModFolder = "BepInEx/config/" + BuildConfig.PackageID + "";
-
-        public static bool isInitialized = false;
-
-        public static ManualLogSource Logger;
-
-        private static World _serverWorld;
-        public static World World
-        {
-            get
-            {
-                if (_serverWorld != null) return _serverWorld;
-
-                _serverWorld = GetWorld("Server")
-                    ?? throw new Exception("There is no Server world (yet). Did you install a server mod on the client?");
-                return _serverWorld;
-            }
-        }
-
-        public static bool IsServer => Application.productName == "VRisingServer";
-
-        private static World GetWorld(string name)
-        {
-            foreach (var world in World.s_AllWorlds)
-            {
-                if (world.Name == name)
-                {
-                    return world;
-                }
-            }
-
-            return null;
-        }
-
-        public void InitConfig()
-        {
-            EnableMod = Config.Bind("Config", "Enable Mod", true, "Enable or Disable PotionsHaveContainers");
-        }
+        public static ManualLogSource LogInstance { get; private set; }
 
         public override void Load()
         {
-            InitConfig();
+            LogInstance = Log;
+            Settings.Initialize(Config);
 
-            Logger = Log;
-            harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-
-            Log.LogInfo($"Plugin {BuildConfig.Name}-v{BuildConfig.Version} is loaded!");
-        }
-
-        public override bool Unload()
-        {
-            Config.Clear();
-            harmony.UnpatchSelf();
-            ConsumableToContainerSystem.Deinitialize();
-            return true;
-        }
-
-        [HarmonyPatch(typeof(GameBootstrap), nameof(GameBootstrap.Start))]
-        public static class GameBootstrapStart_Patch
-        {
-            public static void Postfix()
+            if (!VWorld.IsServer)
             {
-                var Plugin = new Plugin();
-                Plugin.OnGameInitialized();
+                Log.LogWarning("This plugin is a server-only plugin.");
             }
         }
 
         public void OnGameInitialized()
         {
-            if (isInitialized) return;
-            _EnableMod = EnableMod.Value;
+            if (VWorld.IsClient)
+            {
+                return;
+            }
+            
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
 
-            isInitialized = true;
+        public override bool Unload()
+        {
+            Config.Clear();
+            _harmony.UnpatchSelf();
+            ConsumableToContainerSystem.Deinitialize();
+            return true;
         }
     }
 }
--- Utils/Helper.cs
+++ Utils/Helper.cs
@@ -1,105 +1,29 @@
-﻿using ProjectM;
-using System;
-using System.Runtime.InteropServices;
+﻿using System;
+using ProjectM;
+using BepInEx.Logging;
+using Bloodstone.API;
 using Unity.Collections;
 using Unity.Entities;
-using Wetstone.API;
 
 namespace PotionsHaveContainers.Utils
 {
     public static class Helper
     {
-        public static Entity TempEntity = new Entity();
-        public static EntityManager em = Plugin.World.EntityManager;
-        private static System.Random rand = new System.Random();
-        private static Entity empty_entity = new Entity();
+        private static ManualLogSource _log => Plugin.LogInstance;
 
-        public static PrefabGUID GetGUIDFromName(string name)
+        public static void AddItemToInventory(Entity recipient, PrefabGUID itemType, int amount)
         {
-            GameDataSystem gameDataSystem = VWorld.Server.GetExistingSystem<GameDataSystem>();
-            ManagedDataRegistry managed = gameDataSystem.ManagedDataRegistry;
-
-            foreach (Unity.Collections.LowLevel.Unsafe.KeyValue<PrefabGUID, ItemData> entry in gameDataSystem.ItemHashLookupMap)
-            {
-                try
-                {
-                    ManagedItemData item = managed.GetOrDefault<ManagedItemData>(entry.Key);
-                    if (item.PrefabName.StartsWith("Item_VBloodSource") || item.PrefabName.StartsWith("GM_Unit_Creature_Base") || item.PrefabName == "Item_Cloak_ShadowPriest") continue;
-                    if (item.Name.ToString().ToLower() == name.ToLower())
-                    {
-                        return entry.Key;
-                    }
-                }
-                catch { }
-            }
-
-            return new PrefabGUID(0);
-        }
-        public static bool AddItemToInventory(EntityManager entityManager, NativeHashMap<PrefabGUID, ItemData> itemDataMap, Entity target, PrefabGUID itemType, int itemStacks, out int remainingStacks, out Entity newEntity, bool dropRemainder = false)
-        {
-            if (!Plugin.IsServer)
-            {
-                remainingStacks = itemStacks;
-                newEntity = Entity.Null;
-                return false;
-            }
-            itemDataMap ??= Plugin.World.GetExistingSystem<GameDataSystem>().ItemHashLookupMap;
-
-            unsafe
-            {
-                // Some hacky code to create a null-able that won't be GC'ed by the IL2CPP domain.
-                var bytes = stackalloc byte[Marshal.SizeOf<FakeNull>()];
-                var bytePtr = new IntPtr(bytes);
-                Marshal.StructureToPtr<FakeNull>(new()
-                {
-                    value = 7,
-                    has_value = true
-                }, bytePtr, false);
-                var boxedBytePtr = IntPtr.Subtract(bytePtr, 0x10);
-                var fakeInt = new Il2CppSystem.Nullable<int>(boxedBytePtr);
-
-                return InventoryUtilitiesServer.TryAddItem(entityManager, itemDataMap, target, itemType, itemStacks, out remainingStacks, out newEntity, startIndex: fakeInt, dropRemainder: dropRemainder);
-            }
-        }
-
-        public static PrefabGUID GetPrefabGUID(Entity entity)
-        {
-            var entityManager = Plugin.World.EntityManager;
-            PrefabGUID guid;
             try
             {
-                guid = entityManager.GetComponentData<PrefabGUID>(entity);
+                var gameData = VWorld.Server.GetExistingSystem<GameDataSystem>();
+                var itemSettings = AddItemSettings.Create(VWorld.Server.EntityManager, gameData.ItemHashLookupMap);
+                //_log.LogDebug($"Trying to add item with GUID: {itemType} to {recipient} with amount {amount}");
+                InventoryUtilitiesServer.TryAddItem(itemSettings, recipient, itemType, amount);
             }
-            catch
+            catch (Exception e)
             {
-                guid.GuidHash = 0;
+                _log.LogError(e.Message);
             }
-            return guid;
-        }
-
-        public static string GetPrefabName(PrefabGUID hashCode)
-        {
-            var s = Plugin.World.GetExistingSystem<PrefabCollectionSystem>();
-            string name = "Nonexistent";
-            if (hashCode.GuidHash == 0)
-            {
-                return name;
-            }
-            try
-            {
-                name = s.PrefabNameLookupMap[hashCode].ToString();
-            }
-            catch
-            {
-                name = "NoPrefabName";
-            }
-            return name;
-        }
-
-        struct FakeNull
-        {
-            public int value;
-            public bool has_value;
         }
     }
 }
